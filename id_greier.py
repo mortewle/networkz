417eb7e896559d8a0d1875cc5657ed4f6ccbce42
mortewle <70330502+mortewle@users.noreply.github.com>
2022-12-13 08:05:56 +0100

    Update id_greier.py

--- id_greier.py
+++ id_greier.py
@@ -1,33 +1,5 @@
 import numpy as np
 
-# funksjon som sjekker om id-kolonnene finnes, eller om geometri (wkt) skal brukes
-# returnerer tuple med kolonnenavn for start- og sluttpunktene
-def bestem_ids(id_kolonne, startpunkter, sluttpunkter=None) -> tuple:
-    if id_kolonne is None:
-        return ("geom_wkt", "geom_wkt")
-    
-    elif isinstance(id_kolonne, str):
-        if id_kolonne=="geometry":
-            return ("geom_wkt", "geom_wkt")
-        if sluttpunkter is not None:
-            if id_kolonne in startpunkter.columns and id_kolonne in sluttpunkter.columns:
-                return (id_kolonne, id_kolonne)
-            else:
-                raise ValueError("id_kolonne finnes ikke i start- og/eller sluttpunkt-dataene")
-        if id_kolonne in startpunkter.columns:
-            return (id_kolonne, id_kolonne)
-        else:
-            raise ValueError("id_kolonne finnes ikke i startpunkt-dataene")
-    
-    elif isinstance(id_kolonne, list) or isinstance(id_kolonne, tuple) and len(id_kolonne)==2:
-        if id_kolonne[0] in startpunkter.columns and id_kolonne[1] in sluttpunkter.columns:
-            return (id_kolonne[0], id_kolonne[1])
-        else:
-            raise ValueError("id_kolonne finnes ikke i start- eller sluttpunkt-dataene")
-    
-    else:
-        raise ValueError("id_kolonne er verken None, string, liste eller tuple.")
-
 
 def lag_midlr_id(noder, startpunkter, sluttpunkter=None):
     startpunkter["nz_idx"] = range(len(startpunkter))
@@ -64,4 +36,33 @@ def map_ids(df, id_kolonner, startpunkter, sluttpunkter=None):
             id_dict_slutt = {nz_idx: idd.wkt  for idd, nz_idx in zip(sluttpunkter.geometry, sluttpunkter["nz_idx"])}
             df["til"] = df["til"].map(id_dict_slutt)
             
-    return df
+    return df.reset_index(drop=True)
+
+
+# funksjon som sjekker om id-kolonnene finnes, eller om geometri (wkt) skal brukes
+# returnerer tuple med kolonnenavn for start- og sluttpunktene
+def bestem_ids(id_kolonne, startpunkter, sluttpunkter=None) -> tuple:
+    if id_kolonne is None:
+        return ("geom_wkt", "geom_wkt")
+    
+    elif isinstance(id_kolonne, str):
+        if id_kolonne=="geometry":
+            return ("geom_wkt", "geom_wkt")
+        if sluttpunkter is not None:
+            if id_kolonne in startpunkter.columns and id_kolonne in sluttpunkter.columns:
+                return (id_kolonne, id_kolonne)
+            else:
+                raise ValueError("id_kolonne finnes ikke i start- og/eller sluttpunkt-dataene")
+        if id_kolonne in startpunkter.columns:
+            return (id_kolonne, id_kolonne)
+        else:
+            raise ValueError("id_kolonne finnes ikke i startpunkt-dataene")
+    
+    elif isinstance(id_kolonne, list) or isinstance(id_kolonne, tuple) and len(id_kolonne)==2:
+        if id_kolonne[0] in startpunkter.columns and id_kolonne[1] in sluttpunkter.columns:
+            return (id_kolonne[0], id_kolonne[1])
+        else:
+            raise ValueError("id_kolonne finnes ikke i start- eller sluttpunkt-dataene")
+    
+    else:
+        raise ValueError("id_kolonne er verken None, string, liste eller tuple.")
