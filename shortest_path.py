1448b3ea28997300923d0bd71afd7c289f623feb
mortewle <70330502+mortewle@users.noreply.github.com>
2022-12-29 21:31:49 +0100

    parameter-krøll

--- shortest_path.py
+++ shortest_path.py
@@ -4,8 +4,6 @@ import numpy as np
 from networkz.hjelpefunksjoner import gdf_concat
 from networkz.id_greier import bestem_ids, lag_midlr_id, map_ids
 from networkz.lag_igraph import lag_graf
-from shapely.geometry import LineString
-
 
 
 def shortest_path(G,
@@ -24,7 +22,7 @@ def shortest_path(G,
     startpunkter = startpunkter.copy().to_crs(25833)
     sluttpunkter = sluttpunkter.copy().to_crs(25833)
     G.nettverk = G.nettverk.to_crs(25833)
-
+    
     id_kolonner = bestem_ids(id_kolonne, startpunkter, sluttpunkter)
 
     startpunkter["nz_idx"], sluttpunkter["nz_idx"] = lag_midlr_id(G.noder, startpunkter, sluttpunkter)
@@ -35,49 +33,44 @@ def shortest_path(G,
                                               sluttpunkter)
     
     # funksjonen get_shortest_paths() må loopes for hver fra-til-kombinasjon
-    def kjor_korteste(start, slutt, G, G2, tell_opp):
-        
-        if len(start)==0 or len(slutt)==0:
-            return
-        
+    def kjor_korteste(fra_id, til_id, G, G2, tell_opp):
+                
         res = G2.get_shortest_paths(weights='weight', 
                                     v = fra_id, 
                                     to = til_id)
-        
+                
         if len(res[0])==0:
-            linjer.append(gpd.GeoDataFrame(pd.DataFrame({"fra": [fra_id], "til": [til_id], G.kostnad: [np.nan], "geometry": LineString()}), geometry="geometry", crs=25833))
             return []
-        
+
         path = G2.vs[res[0]]["name"]
-        
+                
         if tell_opp:
             return [pd.DataFrame({"source_target": (str(source) + "_" + str(target) for source, target in zip(path[:-1], path[1:]))})]
         
         linje = G.nettverk.loc[(G.nettverk.source.isin(path)) & (G.nettverk.target.isin(path)), ["geometry"]]
+        
+        if not len(linje):
+            return []
         
         linje = linje.dissolve()
         
         linje["fra"] = fra_id
         linje["til"] = til_id
         
-        # for å få kostnaden også:
-        kost = G2.distances(weights='weight', source = start["nz_idx"], target = slutt["nz_idx"])
+        # for å få kostnaden også
+        kost = G2.distances(weights='weight', source = fra_id, target = til_id)
         linje[G.kostnad] = kost[0][0]
-        
+
         return [linje]
     
     linjer = []
     if radvis:
         for fra_id, til_id in zip(startpunkter["nz_idx"], sluttpunkter["nz_idx"]):
-            start = startpunkter[startpunkter["nz_idx"]==fra_id]
-            slutt = sluttpunkter[sluttpunkter["nz_idx"]==til_id]
-            linjer = linjer + kjor_korteste(start, slutt, G, G2, tell_opp)
+            linjer = linjer + kjor_korteste(fra_id, til_id, G, G2, tell_opp)
     else:
         for fra_id in startpunkter["nz_idx"]:
             for til_id in sluttpunkter["nz_idx"]:
-                start = startpunkter[startpunkter["nz_idx"]==fra_id]
-                slutt = sluttpunkter[sluttpunkter["nz_idx"]==til_id]
-                linjer = linjer + kjor_korteste(start, slutt, G, G2, tell_opp)
+                linjer = linjer + kjor_korteste(fra_id, til_id, G, G2, tell_opp)
     
     if tell_opp:
         lenker = pd.concat(linjer, ignore_index=True)
